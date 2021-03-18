9add1163b9725cc2570271fb8d13734b8d1f6b53
Kurt Obando <kurtobando.developer@gmail.com>
2021-03-18 09:04:33 +0800

    corrected with eslint, and prettier

--- source/script/script.js
+++ source/script/script.js
@@ -1,105 +1,100 @@
-function Tags( element ) {
-	let DOMParent = element
-	let DOMList
-	let DOMInput
-	let dataAttribute
-	let arrayOfList
-	
-	function DOMCreate() {
-		let ul 		= document.createElement('ul')
-		let li 		= document.createElement('li')
-		let input 	= document.createElement('input')
-		
-		DOMParent.appendChild( ul )
-		DOMParent.appendChild( input )
-		
-		// first child is <ul>
-		DOMList 	= DOMParent.firstElementChild
-		// last child is <input>
-		DOMInput	= DOMParent.lastElementChild
-	}
-	
-	function DOMRender() {
-		// clear the entire <li> inside <ul> 
-		DOMList.innerHTML = ''
-		
-		// render each <li> to <ul>
-		arrayOfList.forEach(( currentValue, index ) => {
-			let li 				= document.createElement('li')
-				li.innerHTML 	= `${currentValue} <a>&times;</a>`
-				li.querySelector('a').addEventListener( 'click', function() {
-					if( confirm('Continue to remove tag?') ){
-						onDelete( index )	 
-					}
+function Tags(element) {
+    let DOMParent = element
+    let DOMList
+    let DOMInput
+    let dataAttribute
+    let arrayOfList
 
-					return false;
-				})
-			
-			DOMList.appendChild( li )
-			setAttribute()
-		})
-		
-		
-	}
-	
-	function onKeyUp() {
-		DOMInput.addEventListener( 'keyup', function( event ) {
-			let text = this.value.trim()
-			
-			// check if ',' or 'enter' key was press
-			if ( text.includes(',') || event.keyCode == 13 ) {
-				// check if empty text when ',' is remove
-				if ( text.replace(',', '') != '') {
-					// push to array and remove ','
-					arrayOfList.push( text.replace(',' , '') )
-				}
-				// clear input
-				this.value = ''
-			}
-			
-			DOMRender()
-		})
-	}
-	
-	function onDelete( id ) {
-		arrayOfList = arrayOfList.filter(function( currentValue, index ) {
-			if (index == id ) {
-				return false
-			}
-			return currentValue
-		})
-		
-		DOMRender()
-	}
-	
-	function getAttribute() {
-		dataAttribute = DOMParent.getAttribute('data-simple-tags')
-		dataAttribute = dataAttribute.split(',')
-		
-		// store array of data attribute in arrayOfList
-		arrayOfList = dataAttribute.map(( currentValue ) => {
-			return currentValue.trim()
-		})
-	}
-	
-	function setAttribute() {
-		DOMParent.setAttribute('data-simple-tags', arrayOfList.toString());
-	}
-	
-	getAttribute()
-	DOMCreate()
-	DOMRender()
-	onKeyUp()
+    function DOMCreate() {
+        let ul = document.createElement("ul")
+        let input = document.createElement("input")
+
+        DOMParent.appendChild(ul)
+        DOMParent.appendChild(input)
+
+        // first child is <ul>
+        DOMList = DOMParent.firstElementChild
+        // last child is <input>
+        DOMInput = DOMParent.lastElementChild
+    }
+
+    function DOMRender() {
+        // clear the entire <li> inside <ul>
+        DOMList.innerHTML = ""
+
+        // render each <li> to <ul>
+        arrayOfList.forEach((currentValue, index) => {
+            let li = document.createElement("li")
+            li.innerHTML = `${currentValue} <a>&times;</a>`
+            li.querySelector("a").addEventListener("click", function () {
+                if (confirm("Continue to remove tag?")) {
+                    onDelete(index)
+                }
+
+                return false
+            })
+
+            DOMList.appendChild(li)
+            setAttribute()
+        })
+    }
+
+    function onKeyUp() {
+        DOMInput.addEventListener("keyup", function (event) {
+            let text = this.value.trim()
+
+            // check if ',' or 'enter' key was press
+            if (text.includes(",") || event.keyCode === 13) {
+                // check if empty text when ',' is remove
+                if (text.replace(",", "") !== "") {
+                    // push to array and remove ','
+                    arrayOfList.push(text.replace(",", ""))
+                }
+                // clear input
+                this.value = ""
+            }
+
+            DOMRender()
+        })
+    }
+
+    function onDelete(id) {
+        arrayOfList = arrayOfList.filter(function (currentValue, index) {
+            if (index === id) {
+                return false
+            }
+            return currentValue
+        })
+
+        DOMRender()
+    }
+
+    function getAttribute() {
+        dataAttribute = DOMParent.getAttribute("data-simple-tags")
+        dataAttribute = dataAttribute.split(",")
+
+        // store array of data attribute in arrayOfList
+        arrayOfList = dataAttribute.map((currentValue) => {
+            return currentValue.trim()
+        })
+    }
+
+    function setAttribute() {
+        DOMParent.setAttribute("data-simple-tags", arrayOfList.toString())
+    }
+
+    getAttribute()
+    DOMCreate()
+    DOMRender()
+    onKeyUp()
 }
 
-
-
 // run immediately
-(function(){
-	let DOMSimpleTags = document.querySelectorAll('.simple-tags')
-		DOMSimpleTags = Array.from( DOMSimpleTags )
-		DOMSimpleTags.forEach( function( currentValue, index ) {
-			// create Tags
-			new Tags( currentValue )
-		})
+;(function () {
+    let DOMSimpleTags = document.querySelectorAll(".simple-tags")
+    DOMSimpleTags = Array.from(DOMSimpleTags)
+    DOMSimpleTags.forEach(function (currentValue) {
+        // create Tags
+        new Tags(currentValue)
+    })
 })()
